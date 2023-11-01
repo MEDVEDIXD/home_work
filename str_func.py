baf099816ed8b72099d1cb7de9da35abc24a084f
Michail Vinogradov <i.have.nothing.against@gmail.com>
2023-11-01 23:12:39 +0500

    добавление новой функции в str_func.py

--- str_func.py
+++ str_func.py
@@ -1,3 +1,12 @@
 def big_line(line):
-    '''принимает на вход строку и возвращает ее со всеми заглавными буквами'''
+    """Принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return str(line).upper()
+
+
+def two_big_letters(line):
+    """Делает заглавными первые буквы каждого слова в строке, поступившей на вход функции"""
+    words = str(line).split()
+    new_line = []
+    for word in words:
+        new_line.append(word[:2].upper() + word[2:])
+    return ' '.join(new_line)
